57fc809d9592a4e167c6df624f6b81bb356f07a1
amthomps1 <amthomps1@gmail.com>
2021-05-26 14:55:59 -0400

    moving things around

--- .eleventy.js
+++ .eleventy.js
@@ -16,7 +16,7 @@ module.exports = config => {
 module.exports = {
     dir: {
         // ⚠️ These values are both relative to your input directory.
-        includes: "_includes",
-        layouts: "_includes/layouts"
+        includes: "src/_includes",
+        layouts: "src/_includes/layouts"
     }
 };
